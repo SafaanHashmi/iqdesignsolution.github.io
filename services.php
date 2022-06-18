dc299dcb880ee893175145c1017a475fef3d5a87
SafaanHashmi <55549354+SafaanHashmi@users.noreply.github.com>
2022-06-19 00:47:03 +0530

    Update services.php

--- services.php
+++ services.php
@@ -100,117 +100,151 @@
       </div>
     </section><!-- End Breadcrumbs -->
 
-    <!-- ======= Services Section ======= -->
+ <!-- ======= Services Section ======= -->
     <section id="services" class="services">
       <div class="container">
-
+		
+		<div class="section-title" data-aos="fade-up">
+          <h2><strong>Services</strong> we provide</h2>
+        </div>
+		
         <div class="row">
-          <div class="col-lg-4 col-md-6">
-            <div class="icon-box" data-aos="fade-up">
-              <div class="icon"><i class="bi bi-briefcase"></i></div>
+          <div class="col-lg-3 col-md-6">
+            <div class="icon-box" data-aos="fade-up" style="height: 120px;">
+              <div class="icon"><i class='bx bx-buildings'></i></div>
               <h4 class="title"><a href="">Exterior Work</a></h4>
-              <p class="description">Voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident</p>
             </div>
           </div>
-          <div class="col-lg-4 col-md-6">
-            <div class="icon-box" data-aos="fade-up" data-aos-delay="100">
-              <div class="icon"><i class="bi bi-card-checklist"></i></div>
+          <div class="col-lg-3 col-md-6">
+            <div class="icon-box" data-aos="fade-up" data-aos-delay="100" style="height: 120px;">
+              <div class="icon"><i class='bx bx-bed'></i></div>
               <h4 class="title"><a href="">Interior Designing</a></h4>
-              <p class="description">Minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat tarad limino ata</p>
             </div>
           </div>
-          <div class="col-lg-4 col-md-6">
-            <div class="icon-box" data-aos="fade-up" data-aos-delay="200">
-              <div class="icon"><i class="bi bi-bar-chart"></i></div>
+          <div class="col-lg-3 col-md-6">
+            <div class="icon-box" data-aos="fade-up" data-aos-delay="200"  style="height: 120px;">
+              <div class="icon"><i class='bx bx-edit-alt'></i></div>
               <h4 class="title"><a href="">House Plan</a></h4>
-              <p class="description">Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur</p>
             </div>
           </div>
-          <div class="col-lg-4 col-md-6">
-            <div class="icon-box" data-aos="fade-up" data-aos-delay="200">
+          <div class="col-lg-3 col-md-6">
+            <div class="icon-box" data-aos="fade-up" data-aos-delay="200" style="height: 120px;">
               <div class="icon"><i class="bi bi-binoculars"></i></div>
-              <h4 class="title"><a href="">Interior Architecture</a></h4>
-              <p class="description">Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum</p>
-            </div>
-          </div>
-          <div class="col-lg-4 col-md-6">
-            <div class="icon-box" data-aos="fade-up" data-aos-delay="300">
-              <div class="icon"><i class="bi bi-brightness-high"></i></div>
-              <h4 class="title"><a href="">Modular Kitchen Work</a></h4>
-              <p class="description">At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque</p>
-            </div>
-          </div>
-          <div class="col-lg-4 col-md-6">
-            <div class="icon-box" data-aos="fade-up" data-aos-delay="400">
-              <div class="icon"><i class="bi bi-calendar4-week"></i></div>
-              <h4 class="title"><a href="">Renovation</a></h4>
-              <p class="description">Et harum quidem rerum facilis est et expedita distinctio. Nam libero tempore, cum soluta nobis est eligendi</p>
+              <h4 class="title"><a href="">Structure</a></h4>
             </div>
           </div>
         </div>
 
       </div>
     </section><!-- End Services Section -->
-
-    <!-- ======= Features Section ======= -->
-    <section id="features" class="features">
+	
+	
+	
+ <!-- ======= Features Section ======= -->
+    <section id="features" class="features" style="margin-top: 30px;">
       <div class="container">
 
         <div class="section-title" data-aos="fade-up">
-          <h2>Some <strong>Features</strong> we do provide</h2>
-          <p>Magnam dolores commodi suscipit. Necessitatibus eius consequatur ex aliquid fuga eum quidem. Sit sint consectetur velit. Quisquam quos quisquam cupiditate. Et nemo qui impedit suscipit alias ea. Quia fugiat sit in iste officiis commodi quidem hic quas.</p>
+          <h4>Click slides for more</h4>
         </div>
 
         <div class="row">
           <div class="col-lg-4 mb-5 mb-lg-0" data-aos="fade-right">
-            <ul class="nav nav-tabs flex-column">
+            <ul class="nav nav-tabs flex-column" style="box-shadow: rgba(0, 0, 0, 0.15) 1.95px 1.95px 2.6px;">
               <li class="nav-item">
                 <a class="nav-link active show" data-bs-toggle="tab" href="#tab-1">
-                  <h4>Modi sit est</h4>
-                  <p>Quis excepturi porro totam sint earum quo nulla perspiciatis eius.</p>
+                  <h4>Exterior</h4>
+                  <p>We Provide all the major exterior services.</p>
                 </a>
               </li>
               <li class="nav-item mt-2">
                 <a class="nav-link" data-bs-toggle="tab" href="#tab-2">
-                  <h4>Unde praesentium sed</h4>
-                  <p>Voluptas vel esse repudiandae quo excepturi.</p>
+                  <h4>Interior</h4>
+                  <p>We Provide all the major interior services.</p>
                 </a>
               </li>
               <li class="nav-item mt-2">
                 <a class="nav-link" data-bs-toggle="tab" href="#tab-3">
-                  <h4>Pariatur explicabo vel</h4>
-                  <p>Velit veniam ipsa sit nihil blanditiis mollitia natus.</p>
+                  <h4>House Plan</h4>
+                  <p>We provide you best house plans according to your land size.</p>
                 </a>
               </li>
               <li class="nav-item mt-2">
                 <a class="nav-link" data-bs-toggle="tab" href="#tab-4">
-                  <h4>Nostrum qui quasi</h4>
-                  <p>Ratione hic sapiente nostrum doloremque illum nulla praesentium id</p>
+                  <h4>Structure</h4>
+                  <p>Variour kind of structural work is also available</p>
                 </a>
               </li>
             </ul>
           </div>
           <div class="col-lg-7 ml-auto" data-aos="fade-left" data-aos-delay="100">
             <div class="tab-content">
-              <div class="tab-pane active show" id="tab-1">
-                <figure>
-                  <img src="assets/img/features-1.png" alt="" class="img-fluid">
-                </figure>
+              <div class="tab-pane active show" id="tab-1" style="padding-top: 2px;">
+				<div style="padding-top: 20px; padding-left: 20px; box-shadow: rgb(0 0 0 / 24%) 0px 3px 8px;">
+					<h4>For exterior we offer following services -:</h4>
+					<div class="row">
+						<div class="col-lg-6 mb-6 mb-lg-0" data-aos="fade-right">
+							<h5>2D Elevation</h5>
+						</div>
+						<div class="col-lg-6 mb-6 mb-lg-0" data-aos="fade-right">
+							<h5>3D Elevation</h5>
+						</div>
+						<div class="col-lg-6 mb-6 mb-lg-0" data-aos="fade-right">
+							<h5>Working Drawing</h5>
+						</div>
+					</div>
+				</div>
               </div>
-              <div class="tab-pane" id="tab-2">
-                <figure>
-                  <img src="assets/img/features-2.png" alt="" class="img-fluid">
-                </figure>
+              <div class="tab-pane" id="tab-2" style="padding-top: 2px;">
+                <div style="padding-top: 20px; padding-left: 20px; box-shadow: rgb(0 0 0 / 24%) 0px 3px 8px;">
+					<h4>For interior we offer following services -:</h4>
+					<div class="row">
+						<div class="col-lg-6 mb-6 mb-lg-0" data-aos="fade-right">
+							<h5>Furniture Layout</h5>
+						</div>
+						<div class="col-lg-6 mb-6 mb-lg-0" data-aos="fade-right">
+							<h5>Wall Elevation</h5>
+						</div>
+						<div class="col-lg-6 mb-6 mb-lg-0" data-aos="fade-right">
+							<h5>Electrical Celling Design</h5>
+						</div>
+						<div class="col-lg-6 mb-6 mb-lg-0" data-aos="fade-right">
+							<h5>3d View</h5>
+						</div>
+					</div>
+				</div>
               </div>
-              <div class="tab-pane" id="tab-3">
-                <figure>
-                  <img src="assets/img/features-3.png" alt="" class="img-fluid">
-                </figure>
+              <div class="tab-pane" id="tab-3" style="padding-top: 2px;">
+                <div style="padding-top: 20px; padding-left: 20px; box-shadow: rgb(0 0 0 / 24%) 0px 3px 8px;">
+					<h4>We provide 2 types of floor plans-:</h4>
+					<div class="row">
+						<div class="col-lg-6 mb-6 mb-lg-0" data-aos="fade-right">
+							<h5>Floor Plan</h5>
+						</div>
+						<div class="col-lg-6 mb-6 mb-lg-0" data-aos="fade-right">
+							<h5>3D Floor Plan</h5>
+						</div>
+					</div>
+				</div>
               </div>
-              <div class="tab-pane" id="tab-4">
-                <figure>
-                  <img src="assets/img/features-4.png" alt="" class="img-fluid">
-                </figure>
+              <div class="tab-pane" id="tab-4" style="padding-top: 2px;">
+                <div style="padding-top: 20px; padding-left: 20px; box-shadow: rgb(0 0 0 / 24%) 0px 3px 8px;">
+					<h4>Structural Services are as follows -:</h4>
+					<div class="row">
+						<div class="col-lg-6 mb-6 mb-lg-0" data-aos="fade-right">
+							<h5>Coloumn Layout</h5>
+						</div>
+						<div class="col-lg-6 mb-6 mb-lg-0" data-aos="fade-right">
+							<h5>Coulumn Center Line</h5>
+						</div>
+						<div class="col-lg-6 mb-6 mb-lg-0" data-aos="fade-right">
+							<h5>Functional Celling Design</h5>
+						</div>
+						<div class="col-lg-6 mb-6 mb-lg-0" data-aos="fade-right">
+							<h5>3d View</h5>
+						</div>
+					</div>
+				</div>
               </div>
             </div>
           </div>
@@ -218,6 +252,11 @@
 
       </div>
     </section><!-- End Features Section -->
+
+
+   
+
+   
   
 	
 	
@@ -235,11 +274,9 @@
           <div class="col-lg-4 col-md-6 footer-contact">
             <h3>IQ Design Solution</h3>
             <p>
-              XYZ Street <br>
-              Indore, MP 535022<br>
-              India<br><br>
-              <strong>Phone:</strong> +91 9988998899<br>
-              <strong>Email:</strong> xyz@gmail.com<br>
+              
+              <strong>Phone:</strong>&nbsp;+91 96444-67873<br>
+              <strong>Email:</strong>&nbsp;&nbsp;  iq95designer@gmail.com<br>
             </p>
           </div>
 
@@ -255,10 +292,21 @@
           </div>
 
           <div class="col-lg-4 col-md-6 footer-newsletter">
-            <h4>Email Us</h4>
-            <form action="https://mail.google.com/mail/?view=cm&fs=1&to=iqsolutionsindore@gmail.com" method="post" style="margin-top: 10px;">
-              <input type="email" name="email" placeholder="Enter you email"><input type="submit" value="Subscribe">
-            </form>
+			<div class="row">
+				<h4>Email Us at &nbsp; : &nbsp;  <i>iq95designer@gmail.com</i></h4> 
+				<form action="https://mail.google.com/mail/?view=cm&fs=1&to=iq95designer@gmail.com" method="post" style="margin-top: 10px;width: 95%;margin-left: 7px;">
+				  <input type="email" name="email" placeholder="Enter you email"><input type="submit" value="Mail">
+				</form>
+			</div>
+			<div class="row">
+				<div class="social-links text-center text-md-right pt-3 pt-md-0" style="margin-top: 30px;">
+					<a href="#" class="twitter"><i class="bx bxl-twitter"></i></a>
+					<a href="#" class="facebook"><i class="bx bxl-facebook"></i></a>
+					<a href="#" class="instagram"><i class="bx bxl-instagram"></i></a>
+					<a href="#" class="google-plus"><i class="bx bxl-skype"></i></a>
+					<a href="#" class="linkedin"><i class="bx bxl-linkedin"></i></a>
+				</div>
+			</div>
           </div>
 
         </div>
@@ -267,9 +315,9 @@
 
     <div class="container d-md-flex py-4">
 
-      <div class="me-md-auto text-center text-md-start">
+      <!--<div class="me-md-auto text-center text-md-start">
         <div class="copyright">
-        <!--  &copy; Copyright <strong><span></span></strong>
+          &copy; Copyright <strong><span></span></strong>
         </div>
         <div class="credits">
          <a href="https://bootstrapmade.com/"></a>
@@ -281,7 +329,7 @@
         <a href="#" class="instagram"><i class="bx bxl-instagram"></i></a>
         <a href="#" class="google-plus"><i class="bx bxl-skype"></i></a>
         <a href="#" class="linkedin"><i class="bx bxl-linkedin"></i></a>
-      </div>
+      </div>-->
     </div>
   </footer><!-- End Footer -->
 

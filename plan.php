8e7d93747235cdc9fb4b340c54b442564a166663
SafaanHashmi <55549354+SafaanHashmi@users.noreply.github.com>
2022-06-19 17:18:23 +0530

    Add files via upload

---
+++ plan.php
@@ -0,0 +1,282 @@
+<!--#cfbe9a cream-->
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="utf-8">
+  <meta content="width=device-width, initial-scale=1.0" name="viewport">
+
+  <title>IQ Design Solution Indore </title>
+  <meta content="" name="description">
+  <meta content="" name="keywords">
+
+  <!-- Favicons -->
+  <link href="assets/img/favicon.png" rel="icon">
+  <link href="assets/img/apple-touch-icon.png" rel="apple-touch-icon">
+
+  <!-- Google Fonts -->
+  <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Muli:300,300i,400,400i,500,500i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet">
+
+  <!-- Vendor CSS Files -->
+  <link href="assets/vendor/animate.css/animate.min.css" rel="stylesheet">
+  <link href="assets/vendor/aos/aos.css" rel="stylesheet">
+  <link href="assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+  <link href="assets/vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">
+  <link href="assets/vendor/boxicons/css/boxicons.min.css" rel="stylesheet">
+  <link href="assets/vendor/remixicon/remixicon.css" rel="stylesheet">
+  <link href="assets/vendor/glightbox/css/glightbox.min.css" rel="stylesheet">
+  <link href="assets/vendor/swiper/swiper-bundle.min.css" rel="stylesheet">
+  <!-- Template Main CSS File -->
+  <link href="assets/css/style.css" rel="stylesheet">
+  <!--Font Links-->
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+  <link href="https://fonts.googleapis.com/css2?family=Indie+Flower&display=swap" rel="stylesheet">
+  
+  <style>
+	@media (max-width: 750px) {
+	  .hide-when-narrow {
+		display: none;
+	  }
+	}
+	@media (min-width: 751px) {
+	  .hide-when-wide {
+		display: none;
+	  }
+	}
+  </style>
+
+</head>
+
+<body>
+
+  <!-- ======= Header ======= -->
+  <header id="header" class="d-flex align-items-center">
+    <div class="container d-flex justify-content-between">
+
+      <div class="logo">
+        <h1 class="text-light"><a href="index.php">IQ Design Solution</a></h1>
+        <!-- Uncomment below if you prefer to use an image logo -->
+        <!-- <a href="index.html"><img src="assets/img/logo.png" alt="" class="img-fluid"></a>-->
+      </div>
+
+      <nav id="navbar" class="navbar">
+        <ul>
+          <li><a class="hover-underline-animation" href="index.php">Home</a></li>
+          <li><a class="hover-underline-animation" href="about.php">About</a></li>
+          <li><a class="hover-underline-animation" href="services.php">Services</a></li>
+          <li><a class="hover-underline-animation" href="youtube.php">Youtube</a></li>
+          <li class="dropdown"><a class="active" href="#"><span>Portfolio</span> <i class="bi bi-chevron-down"></i></a>
+            <ul>
+				<li><a href="interior.php">Interior</a></li>
+                <li><a href="exterior.php">Exterior</a></li>
+                <li><a href="plan.php">Floor Plan</a></li>
+            </ul>
+          </li>
+          <li><a class="hover-underline-animation" href="process.php">Process</a></li>
+          <li><a class="hover-underline-animation" href="contact.php">Contact</a></li>
+        </ul>
+        <i class="bi bi-list mobile-nav-toggle"></i>
+      </nav><!-- .navbar -->
+
+    </div>
+  </header>
+  <!-- End Header -->
+  
+  
+  <main id="main">
+	
+	<!-- ======= Breadcrumbs ======= -->
+    <section id="breadcrumbs" class="breadcrumbs">
+      <div class="container">
+
+        <div class="d-flex justify-content-between align-items-center">
+          <h2>Floor Plan Gallery</h2>
+        </div>
+
+      </div>
+    </section><!-- End Breadcrumbs -->
+	
+	<!-- ======= Portfolio Section ======= -->
+    <section id="portfolio" class="portfolio">
+      <div class="container">
+	  
+	    <div class="section-title" data-aos="fade-up" style="padding-bottom: 5px;">
+			<h2 style="text-align: left;"> <strong><i class="ri-star-line"></i> Floor Plan </strong> Project 1 </h2>
+        </div>
+        <div class="row portfolio-container" data-aos="fade-up">
+          <div class="col-lg-4 col-md-6 portfolio-item">
+			<a href="assets/img/portfolio/plan-main/plan-1.1-main-full.jpg" data-gallery="portfolioGallery" class="portfolio-lightbox preview-link">
+				<img src="assets/img/portfolio/plan-main/plan-1.1-main-full.jpg" class="img-fluid image-info" alt="">
+				<div class="portfolio-info">
+					<h2 style="color: black; text-align: center;">Click for full image</h2>
+				</div>
+			</a>
+          </div>
+          <div class="col-lg-4 col-md-6 portfolio-item">
+			<a href="assets/img/portfolio/plan-main/plan-1.2-main-full.jpg" data-gallery="portfolioGallery" class="portfolio-lightbox preview-link">
+				<img src="assets/img/portfolio/plan-main/plan-1.2-main-full.jpg" class="img-fluid image-info" alt="">
+				<div class="portfolio-info">
+					<h2 style="color: black; text-align: center;">Click for full image</h2>
+				</div>
+			</a>
+          </div>
+		  <div class="col-lg-4 col-md-6 portfolio-item">
+			<a href="assets/img/portfolio/plan-main/plan-1.3-main-full.jpg" data-gallery="portfolioGallery" class="portfolio-lightbox preview-link">
+				<img src="assets/img/portfolio/plan-main/plan-1.3-main-full.jpg" class="img-fluid image-info" alt="">
+				<div class="portfolio-info">
+					<h2 style="color: black; text-align: center;">Click for full image</h2>
+				</div>
+			</a>
+          </div>
+          <div class="col-lg-4 col-md-6 portfolio-item">
+			<a href="assets/img/portfolio/plan-main/plan-1.4-main-full.jpg" data-gallery="portfolioGallery" class="portfolio-lightbox preview-link">
+				<img src="assets/img/portfolio/plan-main/plan-1.4-main-full.jpg" class="img-fluid image-info" alt="">
+				<div class="portfolio-info">
+					<h2 style="color: black; text-align: center;">Click for full image</h2>
+				</div>
+			</a>
+          </div>
+        </div>
+	  	
+		
+		<div class="section-title" data-aos="fade-up" style="padding-bottom: 5px;">
+			<h2 style="text-align: left;"> <strong><i class="ri-star-line"></i> Furniture </strong> Model </h2>
+        </div>
+        <div class="row portfolio-container" data-aos="fade-up">
+          <div class="col-lg-4 col-md-6 portfolio-item">
+			<a href="assets/img/portfolio/plan-main/plan-2.1-main-full.jpg" data-gallery="portfolioGallery" class="portfolio-lightbox preview-link">
+				<img src="assets/img/portfolio/plan-main/plan-2.1-main-full.jpg" class="img-fluid image-info" alt="">
+			</a>
+          </div>
+          <div class="col-lg-4 col-md-6 portfolio-item">
+			<a href="assets/img/portfolio/plan-main/plan-2.2-main-full.jpg" data-gallery="portfolioGallery" class="portfolio-lightbox preview-link">
+				<img src="assets/img/portfolio/plan-main/plan-2.2-main-full.jpg" class="img-fluid image-info" alt="">
+			</a>
+          </div>
+        </div>
+		
+		
+		
+		<div class="section-title" data-aos="fade-up" style="padding-bottom: 5px;">
+			<h2 style="text-align: left;"> <strong><i class="ri-star-line"></i> Planning </strong> Models</h2>
+        </div>
+        <div class="row portfolio-container" data-aos="fade-up">
+          <div class="col-lg-4 col-md-6 portfolio-item">
+			<a href="assets/img/portfolio/plan-main/plan-3.1-main-full.jpg" data-gallery="portfolioGallery" class="portfolio-lightbox preview-link">
+				<img src="assets/img/portfolio/plan-main/plan-3.1-main-full.jpg" class="img-fluid image-info" alt="">
+			</a>
+          </div>
+          <div class="col-lg-4 col-md-6 portfolio-item">
+			<a href="assets/img/portfolio/plan-main/plan-3.2-main-full.jpg" data-gallery="portfolioGallery" class="portfolio-lightbox preview-link">
+				<img src="assets/img/portfolio/plan-main/plan-3.2-main-full.jpg" class="img-fluid image-info" alt="">
+			</a>
+          </div>
+        </div>
+		
+		
+		
+		<div class="section-title" data-aos="fade-up" style="padding-bottom: 5px;">
+			<h2 style="text-align: left;"> <strong><i class="ri-star-line"></i> Detailed </strong> View</h2>
+        </div>
+        <div class="row portfolio-container" data-aos="fade-up">
+          <div class="col-lg-4 col-md-6 portfolio-item">
+			<a href="assets/img/portfolio/plan-main/plan-4.1-main-full.jpg" data-gallery="portfolioGallery" class="portfolio-lightbox preview-link">
+				<img src="assets/img/portfolio/plan-main/plan-4.1-main-full.jpg" class="img-fluid image-info" alt="">
+			</a>
+          </div>
+        </div>
+		
+      </div>
+    </section><!-- End Portfolio Section -->
+
+  </main>
+  <!-- End #main -->
+
+    
+ <!-- ======= Footer ======= -->
+  <footer id="footer">
+
+    <div class="footer-top">
+      <div class="container">
+        <div class="row">
+
+          <div class="col-lg-4 col-md-6 footer-contact">
+            <h3>IQ Design Solution</h3>
+            <p>
+              
+              <strong>Phone:</strong>&nbsp;+91 96444-67873<br>
+              <strong>Email:</strong>&nbsp;&nbsp;  iq95designer@gmail.com<br>
+            </p>
+          </div>
+
+          <div class="col-lg-4 col-md-6 footer-links">
+            <h4>Useful Links</h4>
+            <ul>
+              <li><i class="bx bx-chevron-right"></i> <a href="index.php">Home</a></li>
+              <li><i class="bx bx-chevron-right"></i> <a href="about.php">About us</a></li>
+              <li><i class="bx bx-chevron-right"></i> <a href="services.php">Services</a></li>
+              <li><i class="bx bx-chevron-right"></i> <a href="#">Terms of service</a></li>
+              <li><i class="bx bx-chevron-right"></i> <a href="#">Privacy policy</a></li>
+            </ul>
+          </div>
+
+          <div class="col-lg-4 col-md-6 footer-newsletter">
+			<div class="row">
+				<h4>Email Us at &nbsp; : &nbsp;  <i>iq95designer@gmail.com</i></h4> 
+				<form action="https://mail.google.com/mail/?view=cm&fs=1&to=iq95designer@gmail.com" method="post" style="margin-top: 10px;width: 95%;margin-left: 7px;">
+				  <input type="email" name="email" placeholder="Enter you email"><input type="submit" value="Mail">
+				</form>
+			</div>
+			<div class="row">
+				<div class="social-links text-center text-md-right pt-3 pt-md-0" style="margin-top: 30px;">
+					<a href="#" class="twitter"><i class="bx bxl-twitter"></i></a>
+					<a href="#" class="facebook"><i class="bx bxl-facebook"></i></a>
+					<a href="#" class="instagram"><i class="bx bxl-instagram"></i></a>
+					<a href="#" class="google-plus"><i class="bx bxl-skype"></i></a>
+					<a href="#" class="linkedin"><i class="bx bxl-linkedin"></i></a>
+				</div>
+			</div>
+          </div>
+
+        </div>
+      </div>
+    </div>
+
+    <div class="container d-md-flex py-4">
+
+      <!--<div class="me-md-auto text-center text-md-start">
+        <div class="copyright">
+          &copy; Copyright <strong><span></span></strong>
+        </div>
+        <div class="credits">
+         <a href="https://bootstrapmade.com/"></a>
+        </div>
+      </div>
+      <div class="social-links text-center text-md-right pt-3 pt-md-0">
+        <a href="#" class="twitter"><i class="bx bxl-twitter"></i></a>
+        <a href="#" class="facebook"><i class="bx bxl-facebook"></i></a>
+        <a href="#" class="instagram"><i class="bx bxl-instagram"></i></a>
+        <a href="#" class="google-plus"><i class="bx bxl-skype"></i></a>
+        <a href="#" class="linkedin"><i class="bx bxl-linkedin"></i></a>
+      </div>-->
+    </div>
+  </footer><!-- End Footer -->
+
+  <a href="#" class="back-to-top d-flex align-items-center justify-content-center"><i class="bi bi-arrow-up-short"></i></a>
+
+  <!-- Vendor JS Files -->
+  <script src="assets/vendor/aos/aos.js"></script>
+  <script src="assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+  <script src="assets/vendor/glightbox/js/glightbox.min.js"></script>
+  <script src="assets/vendor/isotope-layout/isotope.pkgd.min.js"></script>
+  <script src="assets/vendor/swiper/swiper-bundle.min.js"></script>
+  <script src="assets/vendor/waypoints/noframework.waypoints.js"></script>
+  <script src="assets/vendor/php-email-form/validate.js"></script>
+
+  <!-- Template Main JS File -->
+  <script src="assets/js/main.js"></script>
+
+</body>
+
+</html>
